a472f5632b3146aa2616d501fa0cacf748c1abac
inesbssld <iboussel@yahoo.fr>
2023-08-19 19:15:05 +0200

    installation sass etfadein entrée et section oscar via get_template

---
+++ template-parts/oscar.php
@@ -0,0 +1,13 @@
+<section id="oscar" class="oscar">
+    <div class="oscar__texte">
+        <p>Fleurs d’oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</p>
+    </div>
+    <div class="oscar__logo">
+        <img src="wp-content/themes/foce-child/assets/images/oscar_logo.png"
+            alt="Logo des oscars pour le studio koukaki" title="logo des oscars short film animated">
+    </div>
+
+
+
+
+</section>
